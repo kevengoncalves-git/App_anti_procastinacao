998c7a829ae0ebe44f4817344229440294d07366
Keven <kevenkaue25@gmail.com>
2026-03-20 12:51:31 -0300

    Criação dos frames principais

--- main_app.py
+++ main_app.py
@@ -1,17 +1,21 @@
 import customtkinter
 
-print("Esse é meu app anti-procastinação")
+"""print("Esse é meu app anti-procastinação")
 
 
 class FramePrimario(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         self.checkbox1 = customtkinter.CTkCheckBox(self, text="Ta rico?")
-        self.checkbox1.grid(row=0, column=0, padx=10, pady=(0,10), sticky='w')
+        self.checkbox1.grid(row=1, column=0, padx=10, pady=(0,10), sticky='w')
         self.checkbox2 = customtkinter.CTkCheckBox(self, text="Ta liso?")
-        self.checkbox2.grid(row=1, column=0, padx=10, pady=(0,10), sticky='w')
+        self.checkbox2.grid(row=2, column=0, padx=10, pady=(0,10), sticky='w')
         self.checkbox3 = customtkinter.CTkCheckBox(self, text="Só quer mamão so quer mel?")
-        self.checkbox3.grid(row=2, column=0, padx=10, pady=(0,10), sticky='w')
+        self.checkbox3.grid(row=3, column=0, padx=10, pady=(0,10), sticky='w')
+
+        #Add um titulo no meu frame
+        self.titulo = customtkinter.CTkLabel(self, text="Meu titulo", bg_color="gray")
+        self.titulo.grid(row=0, column=0, padx=10, pady=(0,10), sticky='ew')
 
     def get(self):
         if self.checkbox1.get() == 1:
@@ -44,6 +48,49 @@ class APP(customtkinter.CTk):
     def resposta_primeiro_botao(self):
         self.frame_principal.get()
 
+
+app = APP()
+app.mainloop()"""
+
+class FrameTituloInicial(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master, fg_color='#F7DECE', width=200, height=20, border_width=2)
+
+        #Minha fonte
+        self.fonte_personalizada = customtkinter.CTkFont(family='Roboto', size=18, weight='bold')
+
+        self.columnconfigure(0, weight=1)
+
+        self.label_titulo = customtkinter.CTkLabel(self, text="Aplicativo anti-procastinação", 
+                                                   font=self.fonte_personalizada, text_color='black')
+        self.label_titulo.grid(row=0, column=0, padx=3, pady=3, ipadx=20)
+
+
+
+class FramePrincipal(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master, fg_color='#536D88', corner_radius=20)
+
+        self.columnconfigure(0, weight=1)
+
+        self.frame_titulo_inicial = FrameTituloInicial(self)
+        self.frame_titulo_inicial.grid(row=0, column=0, padx=10, pady=10)
+
+
+class APP(customtkinter.CTk):
+    def __init__(self):
+        super().__init__(fg_color='#0F132E')
+
+        #Informações sobre o aplicativo
+        self.title("ANTI-PROCASTINATOR 1.0")
+        self.geometry("400x320")
+        self.resizable(False, False)
+
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        self.frame_principal = FramePrincipal(self)
+        self.frame_principal.grid(row=0,column=0, padx=20, pady=20, sticky='ewns')
 
 app = APP()
 app.mainloop()
